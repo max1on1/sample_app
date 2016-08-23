20f9dcfbc9b10bc839810266eb9ea469d20e2327
maxon <2630161@gmail.com>
2016-08-23 13:59:24 +0000

    add show delet admins etc..

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,6 +6,10 @@ module SessionsHelper
   end
 
     # Returns the current logged-in user (if any).
+  def current_user?(user)
+    user == current_user
+  end
+
   def current_user
      if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
@@ -33,10 +37,15 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
-    def forget(user)
+   def forget(user)
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
   end
 
   # Logs out the current user.
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,5 +6,5 @@ module UsersHelper
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}.png?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
-end
+  end
 end
