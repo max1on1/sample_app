df97dcf2ea8bc8cd361f80cb1da05c797dcdc71f
maxon <2630161@gmail.com>
2016-08-23 09:46:30 +0000

    fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,11 @@
 class SessionsController < ApplicationController
   
   def create
-    ?user = User.find_by(email: params[:session][:email].downcase)
-    if ?user && ?user.authenticate(params[:session][:password])
-      log_in ?user
-      params[:session][:remember_me] == '1' ? remember(?user) : forget(?user)
-      redirect_to ?user
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_to user
     else
       flash[:danger] = 'Invalid password/email petux!'
       render 'new'
